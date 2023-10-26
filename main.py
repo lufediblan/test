cef6e0eda79cf96d5791993ca15cd50ef60bf628
Luis <lufediblan@gmail.com>
2023-10-25 19:22:20 -0500

    2do

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-balance = 845.70
 
-while True:
-    try:
-        num = float(input("Deposit: "))
-        break
-    except ValueError:
-        print("Must be a valid quantity.")
+def Balance():
+    balance = 845.70
 
-balance += num
-print(f"Balance: {balance}")
+    while True:
+        try:
+            num = float(input("Deposit: "))
+            break
+        except ValueError:
+            return ("Must be a valid quantity.")
+
+    balance += num
+    return (f"Balance: {balance}")
+
+
+print(Balance())
