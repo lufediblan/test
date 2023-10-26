7821376df3281f7f968fb9975d6128979420f28b
Luis <lufediblan@gmail.com>
2023-10-25 19:17:48 -0500

    my first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+balance = 845.70
+
+while True:
+    try:
+        num = float(input("Deposit: "))
+        break
+    except ValueError:
+        print("Must be a valid quantity.")
+
+balance += num
+print(f"Balance: {balance}")
